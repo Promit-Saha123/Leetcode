4a18c0d88dc70944a7c313615d72a1b6aed8da27
Promit <70390966+Promit-Saha123@users.noreply.github.com>
2026-08-31 18:24:18 -0400

    Time: 31 ms (19.92%) | Memory: 18.3 MB (9.25%) - LeetSync

---
+++ 49-group-anagrams/group-anagrams.py
@@ -0,0 +1,28 @@
+class Solution(object):
+    def groupAnagrams(self, strs):
+        """
+        :type strs: List[str]
+        :rtype: List[List[str]]
+        """
+
+        groups = {}
+        
+        for word in strs:
+
+            count = [0] * 26 
+
+            for char in word:
+                count[ord(char) - ord("a")] += 1 # running tally of letter frequencies
+            
+            key = tuple(count) # allows as key because it is now immutable
+
+            if key not in groups:
+                groups[key] = []
+
+            groups[key].append(word)
+
+        return list(groups.values()) # converts dictionary values into a list
+
+
+            
+
